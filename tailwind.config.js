fb4a34c63eb9c7b7b3297f6f54f2de555afd2933
Tawhid <433chowdhury@gmail.com>
2022-08-27 01:14:55 +0600

    accent color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "rgba(255,104,77)",
+      },
+    },
   },
   plugins: [],
-}
+};
